b289ccee8f006de87a7bed8ef0409f2c3d7be2dd
xyfLily <13080589961@163.com>
2021-11-08 12:23:02 +0000

    only allow the user ask the same question once

--- legal_move_v1.pl
+++ legal_move_v1.pl
@@ -1,5 +1,5 @@
 :- [deduce_backwards],[why_question],[whynot_question],[write_list].
-:-dynamic node/4, user_fact/2, not_believe/1, believe/1, user_rule/3, different/1.
+:-dynamic node/4, user_fact/2, not_believe/1, believe/1, user_rule/3, different/1, user_question/1.
 
 
 fact(1, "Yes, it is a initial fact").
@@ -13,10 +13,9 @@ reason(2, "It's an initial fact.").
 
 
 answer(1, "A fact").
-answer(3, "Exit").
+answer(2, "Exit").
 
 % start the conversations
-
 chat:-
     write_node_list,!,
     write_rule_list,!,
@@ -28,6 +27,7 @@ print_welcome:-
     write("Computer: What do you want to know?"), nl,
     print_prompt(user),
     read(F),
+    assert(user_question(F)),!,
     (
         deduce_backwards(F,node(_ID, F, _R, _DAG))
         -> print_prompt(bot),write(F), write(' is true.'), nl, ! ,
@@ -72,34 +72,38 @@ read_agree :-
 
 
 read_answer(Nanswer) :-
+    repeat,
     write_answer_list,
     print_prompt(user),
     prompt(_, ''),
     read(Nanswer),
-    (   Nanswer =:= 3
-    ->  print_prompt(bot), write('Bye'),nl,!, halt
-    ;    Nanswer =:= 2
-    ->  print_prompt(bot), write("Please enter the conclusion: "),nl,!
+    (   Nanswer =:= 2
+    ->  print_prompt(bot), write('Bye'),nl,!, halt 
      ;   Nanswer =:= 1
-    ->  print_prompt(bot), write("Please enter the antecedant: "),nl,!
-    ;   write('Not a valid choice, try again...'), nl
-    ),
+    ->  print_prompt(bot), write("Please enter a fact"),nl,!,
+        read_question(_F),!
+    ;   write('Not a valid choice, try again...'), nl,fail
+    ).
+
+
+
+read_question(Fact):-
+    repeat,
     print_prompt(user),
-    read(F),
-    (
-        deduce_backwards(F,node(_ID, F, _R, _DAG))
-        -> print_prompt(bot),write(F), write(' is true.'), nl, ! ,
+    read(Fact),
+    (  
+    user_question(Fact),!
+    ->  write("Computer: You have already asked this fact. Please enter a different fact."),nl,fail
+
+    ;
+        (deduce_backwards(Fact,node(_ID, Fact, _R, _DAG))
+        -> print_prompt(bot),write(Fact), write(' is true.'), nl, ! ,
         read_agree, !,
-        why_question(F),!
-    ;                                 % legal move 4: some t ∈ Gi ∪ Nij the player may ask whynot(t)
-       print_prompt(bot),write(F), write(' is false.'), nl,!,
-       read_agree, !,
-       whynot_question(F),!
-       ).
-
-
-
-
-
-
+        why_question(Fact),!
+        ;                                 % legal move 4: some t ∈ Gi ∪ Nij the player may ask whynot(t)
+        print_prompt(bot),write(Fact), write(' is false.'), nl,!,
+        read_agree, !,
+        whynot_question(Fact),!
+       )
+     ).
 
