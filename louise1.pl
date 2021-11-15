217895c943510c9fcce3ab6df8cddf8183eb46ca
xyfLily <13080589961@163.com>
2021-11-15 21:10:06 +0000

    Add initial question

--- louise1.pl
+++ louise1.pl
@@ -6,6 +6,9 @@ node(3, tier1(manchester), initial_fact, []).
 rule(1,[residence(X, Y), residence(A, B), indoor_meetings_allowed(Y), indoor_meetings_allowed(B)],can_meet_indoors(X, A)).
 rule(2,[tier1(X)], indoor_meetings_allowed(X)).
 
+initial_question(1,can_meet_indoors(mary, karl), "Can Mary and Karl meet indoors?").
 
+%% initial_question(1,can_meet_indoors(X, A)):-
+%%	write("Can"),write(X),write(" and "),write(A),write(" meet indoors?").
 
 
