e30c820af0d0dbd78779436322090a759c823f70
xyfLily <13080589961@163.com>
2021-11-22 11:28:49 +0000

    update

--- legal_move_v1.pl
+++ legal_move_v1.pl
@@ -1,5 +1,9 @@
 :- [deduce_backwards],[why_question],[whynot_question],[write_list].
-:-dynamic node/4, user_fact/2, not_believe/1, believe/1, user_rule/3, different/1, user_question/1.
+:-dynamic node/4, user_fact/2, user_rule/3, different/1, user_question/1,n_computer_user/1,y_computer_user/1,y_user_computer/1,n_user_computer/1,yr_user_computer/1.
+
+option(1,"Computer use a rule that I didn't know").
+option(2,"Ask why about any fact in Y_user_computer that I haven't asked about before").
+option(3, "Ask whynot about any fact in N_user_computer that I haven't asked about before.")
 
 
 fact(1, "Yes, it is a initial fact").
@@ -11,13 +15,15 @@ choice(2, "No, I need more explanations.").
 reason(1, "Because of a rule.").
 reason(2, "It's an initial fact.").
 
+agree(1, "Yes, I agree. Exit.").
+agree(2, "No, I disagree.").
 
 answer(1, "A fact").
 answer(2, "Exit").
 
 question(1,"Can Karl and mary can meet?").
 
-% start the conversations
+
 chat:-
     write_fact_list,!,
     write_rule_list,!,
@@ -26,34 +32,25 @@ chat:-
 
 
 print_welcome:-
-    %% LOUISE:  You should replace these four lines with the question you have picked.
-    %% The question can be coded into the example file  then the computer should start with
-    %% deducing backwards and announce its conclusion and then ask "Do you agree?"
     initial_question(N,Q,Pretty),
     write("Question: "), write(Pretty),nl,
     conclusion(Q).
-    %%print_prompt(user),
-   %% read(F).
-   %% assert(user_question(F)),!,
-   %% (
-   %%     deduce_backwards(F,node(_ID, F, _R, _DAG))
-   %%     -> print_prompt(bot),write(F), write(' is true.'), nl, ! ,
-   %%     read_agree, !,
-        %% LOUISE: At this point the computer should add F to N_computer_user and Y_user_computer
-   %%     why_question(F)
-   %% ;                                 % legal move 4: some t ∈ Gi ∪ Nij the player may ask whynot(t)
-   %%    print_prompt(bot),write(F), write(' is false.'), nl,!,
-   %%    read_agree, !,
-       %% LOUISE: At this point the computer should add F to Y_computer_user and N_user_computer
-   %%    whynot_question(F),!
-    %%   ).
 
 conclusion(F):-
     deduce_backwards(F,node(_ID, F, _R, _DAG))
-    -> print_prompt(bot),print_fact(F), write(' is true.'), write("Do you agree?"),nl, !;
-    print_prompt(bot),write(F), write(' is false.'),write("Do you agree?"), nl,!.
-
-
+    -> print_prompt(bot),print_fact(F), write(' is true.'), write(" Do you agree?"),nl, !,
+       agree,!,
+       assert(n_computer_user(F)),!,
+       assert(y_user_computer(F)),!,
+     %% LOUISE: At this point the computer should add F to N_computer_user and Y_user_computer
+       why_question(F)
+    ;
+        print_prompt(bot),write(F), write(' is false.'),write(" Do you agree?"), nl,!,
+        agree,!,
+        assert(y_computer_user(F)),!,
+        assert(n_user_computer(F)),!,
+       %% LOUISE: At this point the computer should add F to Y_computer_user and N_user_computer
+        whynot_question(F),!.
 
 
 conversations:-
@@ -68,14 +65,21 @@ print_prompt(bot):-
 print_prompt(user):-
         user_icon(X), write(X), write(': '), flush_output.
 
-%% LOUISE: I recommend giving your program a name - e.g., "Covid Advice System" and using that instead of "Computer"
 my_icon("Covid Advice System").
-
-%% LOUISE: I might use "User Response" or "Your Response" here instead of "User"
-user_icon("Your Response").
+user_icon("User Response").
 
 
-
+agree :-
+    write_agree_list,
+    print_prompt(user),
+    prompt(_, ''),
+    read(Nanswer),
+    (   Nanswer =:= 1
+    ->  print_prompt(bot), write('Bye'),nl,!, halt
+    ;   Nanswer =:= 2
+    ->  print_prompt(bot), write("Please select a question or exit: "),nl,!
+    ;   write('Not a valid choice, try again...'), nl
+    ).
 
 
 read_agree :-
