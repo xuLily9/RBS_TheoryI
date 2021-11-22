5f274074b3040b6606c2d9b013f24aca11065c27
xyfLily <13080589961@163.com>
2021-11-22 11:27:50 +0000

    pretty print facts

--- write_list.pl
+++ write_list.pl
@@ -21,7 +21,7 @@ print_fact(Fact):-
 
 write_fact_list :-
     write("FACT: "),nl,
-     node(_N, Fact, initial_fact, []),
+     node(N, Fact, initial_fact, []),
      print_fact(Fact),nl,
      fail.
 write_fact_list.
@@ -48,12 +48,6 @@ write_rule_list.
 
 
 
-
-write_why_list :-
-    write(1), write('. '), write("Why do you beleive?"),nl,
-    write(2), write('. '), write("Exit"), nl.
-
-
 write_whynot_list :-
     write(1), write('. '), write("Why don't you beleive?"),nl,
     write(2), write('. '), write("Exit"), nl.
@@ -64,6 +58,12 @@ write_choice_list :-
     write(N), write('. '), write(Name), nl,
     fail.
 write_choice_list.
+
+write_agree_list :-
+    agree(N, Name),
+    write(N), write('. '), write(Name), nl,
+    fail.
+write_agree_list.
 
 write_reason_list :-
     reason(N, Name),
@@ -78,6 +78,18 @@ write_answer_list :-
     fail.
 write_answer_list.
 
+write_why_list:-
+    y_user_computer(Fact),
+    write(". Why do you beleive "),  write(Fact), write("?"), nl,
+    fail.
+write_why_list.
+
+write_option_list :-
+    option(N, Name),
+    write(N), write('. '), write(Name), nl,
+    fail.
+write_option_list.
+
 
 %% LOUISE:  This should not offer all nodes as a choice.  It should offer everything that is in
 %% Y_user_computer (facts the user (should) think the computer knows) and which the user hasn't
@@ -87,7 +99,7 @@ write_answer_list.
 write_ask_list:-
     %% LOUISE: What if the node was labelled unprovable?
     node(N, Fact, _, _),
-    write(N), write('. '),  write(Fact), nl,
+    write(N), write(". Why do you beleive "), write(Fact), write("?"),nl,
     fail.
 write_ask_list.
 
