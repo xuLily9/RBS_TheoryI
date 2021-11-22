d4eb3ec4d82dd17c968c4b2fc3f6147b23cdc973
xyfLily <13080589961@163.com>
2021-11-22 14:05:30 +0000

    update

--- louise1.pl
+++ louise1.pl
@@ -1,14 +1,25 @@
+%% A set of computer facts and rules 
+
 node(1, residence(mary, manchester), initial_fact,[]).
 node(2, residence(karl, manchester), initial_fact,[]).
 node(3, tier1(manchester), initial_fact, []).
-
 
 rule(1,[residence(X, Y), residence(A, B), indoor_meetings_allowed(Y), indoor_meetings_allowed(B)],can_meet_indoors(X, A)).
 rule(2,[tier1(X)], indoor_meetings_allowed(X)).
 
 initial_question(1,can_meet_indoors(mary, karl), "Can Mary and Karl meet indoors?").
 
-%% initial_question(1,can_meet_indoors(X, A)):-
-%%	write("Can"),write(X),write(" and "),write(A),write(" meet indoors?").
+%% A set of user facts and rules 
+user(1,residence(mary, manchester),initial_fact,[]).
+user(1,residence(karl, stockport),initial_fact,[]).
+user(1,tier2(stockport),initial_fact,[]).
+
+user_rule(1,[residence(X, Y), residence(A, B), indoor_meetings_allowed(Y), indoor_meetings_allowed(B)],can_meet_indoors(X, A)).
+user_rule(2,[tier1(X)], indoor_meetings_allowed(X)).
+
+% user_rule(1,[residence(X, Y), residence(A, B), indoor_meetings_allowed(Y), not(indoor_meetings_allowed(B))],not(can_meet_indoors(X, A))).
+% user_rule(2,[tier2(X)], not(indoor_meetings_allowed(X))).
+
+
 
 
