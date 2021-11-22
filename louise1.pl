2d2c74af3ddc225822d4a974c960607ea23d0763
xyfLily <13080589961@163.com>
2021-11-22 21:54:27 +0000

    update

--- louise1.pl
+++ louise1.pl
@@ -7,12 +7,13 @@ node(3, tier1(manchester), initial_fact, []).
 rule(1,[residence(X, Y), residence(A, B), indoor_meetings_allowed(Y), indoor_meetings_allowed(B)],can_meet_indoors(X, A)).
 rule(2,[tier1(X)], indoor_meetings_allowed(X)).
 
-initial_question(1,can_meet_indoors(mary, karl), "Can Mary and Karl meet indoors?").
+% initial_question(1,can_meet_indoors(mary, karl), "Can Mary and Karl meet indoors?").
+initial_question(1,can_meet_indoors(mary, sue), "Can Mary and sue meet indoors?").
 
 %% A set of user facts and rules 
-user(1,residence(mary, manchester),initial_fact,[]).
-user(1,residence(karl, stockport),initial_fact,[]).
-user(1,tier2(stockport),initial_fact,[]).
+user_fact(1,residence(mary, manchester),initial_fact,[]).
+user_fact(1,residence(karl, stockport),initial_fact,[]).
+user_fact(1,tier2(stockport),initial_fact,[]).
 
 user_rule(1,[residence(X, Y), residence(A, B), indoor_meetings_allowed(Y), indoor_meetings_allowed(B)],can_meet_indoors(X, A)).
 user_rule(2,[tier1(X)], indoor_meetings_allowed(X)).
