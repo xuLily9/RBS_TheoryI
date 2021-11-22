6e0bd8cd2aa51be0a9834e243130d9ad244ffd5c
xyfLily <13080589961@163.com>
2021-11-22 14:04:51 +0000

    update

--- legal_move_v1.pl
+++ legal_move_v1.pl
@@ -1,9 +1,12 @@
 :- [deduce_backwards],[why_question],[whynot_question],[write_list].
-:-dynamic node/4, user_fact/2, user_rule/3, different/1, user_question/1,n_computer_user/1,y_computer_user/1,y_user_computer/1,n_user_computer/1,yr_user_computer/1.
+:-dynamic node/4, user_fact/4, different/1, user_question/1,n_computer_user/1,y_computer_user/1,y_user_computer/1,n_user_computer/1,yr_user_computer/3, asked_question/1.
+
+agree(1, "Yes, I agree. Exit.").
+agree(2, "No, I disagree. I want an explanation.").
 
 option(1,"Computer use a rule that I didn't know").
 option(2,"Ask why about any fact in Y_user_computer that I haven't asked about before").
-option(3, "Ask whynot about any fact in N_user_computer that I haven't asked about before.")
+option(3, "Ask whynot about any fact in N_user_computer that I haven't asked about before.").
 
 
 fact(1, "Yes, it is a initial fact").
@@ -15,8 +18,7 @@ choice(2, "No, I need more explanations.").
 reason(1, "Because of a rule.").
 reason(2, "It's an initial fact.").
 
-agree(1, "Yes, I agree. Exit.").
-agree(2, "No, I disagree.").
+
 
 answer(1, "A fact").
 answer(2, "Exit").
@@ -32,30 +34,65 @@ chat:-
 
 
 print_welcome:-
-    initial_question(N,Q,Pretty),
+    nl,
+    initial_question(_,F,Pretty),
     write("Question: "), write(Pretty),nl,
-    conclusion(Q).
+    conclusion(F).
+
 
 conclusion(F):-
     deduce_backwards(F,node(_ID, F, _R, _DAG))
-    -> print_prompt(bot),print_fact(F), write(' is true.'), write(" Do you agree?"),nl, !,
-       agree,!,
-       assert(n_computer_user(F)),!,
-       assert(y_user_computer(F)),!,
-     %% LOUISE: At this point the computer should add F to N_computer_user and Y_user_computer
-       why_question(F)
+    -> 
+        print_prompt(bot),print_fact(F), write(' is true.'),nl,!,
+        disagree(F),!,
+        assert(n_computer_user(F)),!,     %% LOUISE: At this point the computer should add F to N_computer_user and Y_user_computer
+        assert(y_user_computer(F)),!,  
+        assert(asked_question(F)),!,                                   
+        why(F)
     ;
-        print_prompt(bot),write(F), write(' is false.'),write(" Do you agree?"), nl,!,
-        agree,!,
-        assert(y_computer_user(F)),!,
+        print_prompt(bot),write(F), write(' is false.'),nl,!,
+        disagree(F),!,
+        assert(y_computer_user(F)),!,     %% LOUISE: At this point the computer should add F to Y_computer_user and N_user_computer
         assert(n_user_computer(F)),!,
-       %% LOUISE: At this point the computer should add F to Y_computer_user and N_user_computer
+        assert(asked_question(F)),!,  
         whynot_question(F),!.
 
 
+
+disagree(F):-
+    nl,
+    repeat,
+    print_prompt(bot), write("Do you agree?"),nl,
+    write_agree_list,
+    print_prompt(user),
+    prompt(_, ''),
+    read(N),
+    (   N =:= 1
+    ->  print_prompt(bot), write("Bye"),nl,!, halt
+    ;   N =:= 2
+    ->  print_prompt(user), write("Why do you beleive "), print_fact(F), write("?"), nl,!
+    ;   write("Not a valid choice, try again..."), nl,fail
+    ).
+
+
+option :-
+    print_prompt(bot),
+    write("Please select one of the option:"),nl,
+    write("1. I don't know about this rule used by computer."),nl,
+    write_why_list,
+    print_prompt(user),
+    prompt(_, ''),
+    read(Nanswer),
+    (   Nanswer =:= 1
+    ->  print_prompt(bot), write('Bye'),nl,!, halt
+    ;   Nanswer =:= 2
+    ->  print_prompt(bot), write('Okay, let us move on.'),nl,!
+    ;   write('Not a valid choice, try again...'), nl
+    ).
+
 conversations:-
     repeat,
-    read_agree,  
+    option, 
     write("Computer: What do you want to know?"), nl,
     read_answer(_N),
     different(_),!,halt.
@@ -69,17 +106,6 @@ my_icon("Covid Advice System").
 user_icon("User Response").
 
 
-agree :-
-    write_agree_list,
-    print_prompt(user),
-    prompt(_, ''),
-    read(Nanswer),
-    (   Nanswer =:= 1
-    ->  print_prompt(bot), write('Bye'),nl,!, halt
-    ;   Nanswer =:= 2
-    ->  print_prompt(bot), write("Please select a question or exit: "),nl,!
-    ;   write('Not a valid choice, try again...'), nl
-    ).
 
 
 read_agree :-
