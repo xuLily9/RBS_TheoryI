e9bcd4fc1e20fccb8ee3a0ab47a6510f4440991c
xyfLily <13080589961@163.com>
2021-11-23 21:15:49 +0000

    update

--- louise1.pl
+++ louise1.pl
@@ -7,8 +7,8 @@ node(3, tier1(manchester), initial_fact, []).
 rule(1,[residence(X, Y), residence(A, B), indoor_meetings_allowed(Y), indoor_meetings_allowed(B)],can_meet_indoors(X, A)).
 rule(2,[tier1(X)], indoor_meetings_allowed(X)).
 
-% initial_question(1,can_meet_indoors(mary, karl), "Can Mary and Karl meet indoors?").
-initial_question(1,can_meet_indoors(mary, sue), "Can Mary and sue meet indoors?").
+initial_question(1,can_meet_indoors(mary, karl), "Can Mary and Karl meet indoors?").
+%initial_question(1,can_meet_indoors(mary, sue), "Can Mary and sue meet indoors?").
 
 %% A set of user facts and rules 
 user_fact(1,residence(mary, manchester),initial_fact,[]).
