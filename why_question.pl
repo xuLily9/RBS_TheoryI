d6860c9d0f5deba92fc38b90e39b5a9cced1cd6d
xyfLily <13080589961@163.com>
2021-12-01 20:29:42 +0000

    except not(H)

--- why_question.pl
+++ why_question.pl
@@ -11,7 +11,7 @@ why(F):-
     print_fact(F),
     write("is an initial fact"),nl,
     print_prompt(bot),
-    write("I have identify the difference. Computer believes "), print_fact(F),write(" ,but the user doesn't believe it. Exit."),nl, 
+    write("I have identify the difference. Computer believes "), print_fact(F),write("is an initial fact,but the user doesn't believe it. Exit."),nl, 
     assert(different(F)),!, halt.
   
 why(F):-                                            
