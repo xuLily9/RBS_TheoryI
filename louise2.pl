24ac142ae7518061a1a4f3b283d065057af90833
xyfLily <13080589961@163.com>
2022-01-06 10:56:50 +0000

    update

--- louise2.pl
+++ louise2.pl
@@ -15,7 +15,7 @@ initial_question(2,can_meet_indoors(mary, sue), "Can Mary and sue meet indoors?"
 user_fact(1,residence(mary, manchester),initial_fact,[]).
 user_fact(2,residence(karl, stockport),initial_fact,[]).
 user_fact(3,tier2(stockport),initial_fact,[]).
-
+user_fact(4,tier1(manchester),initial_fact,[]).
 
 user_rule(1,[residence(X, Y), residence(A, B), indoor_meetings_allowed(Y), indoor_meetings_allowed(B), not(symptoms(X)), not(symptoms(A))],can_meet_indoors(X, A)).
 user_rule(2,[tier1(X)], indoor_meetings_allowed(X)).
