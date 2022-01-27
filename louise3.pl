5dea39b0d328e7c59a5ccaaf5aaecf974359d92b
m09238yx <yifan.xu-9@postgrad.manchester.ac.uk>
2022-01-27 11:49:29 +0000

    update

--- louise3.pl
+++ louise3.pl
@@ -8,15 +8,15 @@ node(4, taste_and_smell(mary), initial_fact, []).
 node(5, taste_and_smell(karl), initial_fact, []).
 
 
-rule(1, [residence(X, Y), residence(A, B), indoor_meetings_allowed(Y), indoor_meetings_allowed(B), not(symptoms(X)), not(symptoms(A))],can_meet_indoors(X, A)).
-rule(2, [tier1(X)], indoor_meetings_allowed(X)).
+rule(1,[residence(X, Y), residence(A, B), indoor_meetings_allowed(Y), indoor_meetings_allowed(B), not(symptoms(X)), not(symptoms(A))],can_meet_indoors(X, A)).
+rule(2,[tier1(X)], indoor_meetings_allowed(X)).
 rule(3, [cough(X)], symptoms(X)).
 rule(4, [fever(X)], symptoms(X)).
 rule(5, [not(taste_and_smell(X))], symptoms(X)).
 
 initial_question(1,can_meet_indoors(mary, karl), "Can Mary and Karl meet indoors?").
 
-initial_question(2,symptoms(mary), "Does Mary have symptoms?").
+%%initial_question(2,not(symptoms(mary)), "Does Mary have symptoms?").
 
 %% A set of user facts and rules 
 user_fact(1,residence(mary, manchester),initial_fact,[]).
