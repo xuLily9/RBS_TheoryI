c1d024cacb2a6bca22ceb18bc1e06f038823b5c2
m09238yx <yifan.xu-9@postgrad.manchester.ac.uk>
2022-03-06 20:27:29 +0000

    update

--- whynot_question.pl
+++ whynot_question.pl
@@ -18,21 +18,26 @@ whynot(F):-
     ->  write(Out,'Because it is an user initial fact.'),
         ( 
             user_fact(_,F,initial_fact,_),
-            write('User: Because'),write(Out,'User: Because'), print_fact(F),write(' is an initial fact.'),write(Out,' is an initial fact.'),nl, 
-            assert(y_computer_user(N,F)),!,assert(n_user_computer(N,F)),!,
+            write('User: Because'),write(Out,'User: Because '), print_fact(F),write(' is an initial fact.'),write(Out,' is an initial fact.'),nl, 
+            aggregate_all(count, y_computer_user(_,_), X),
+            aggregate_all(count, n_user_computer(_,_), Y),
+            X1 is X +1,
+            Y1 is Y+1,
+            assert(y_computer_user(X1,F)),!,
+            assert(n_user_computer(Y1,F)),!,
             \+ node(_N, F,_, _), !,
             write('Covid Advice System: I have found the disagreement. User believes '), 
             write(Out, '\n----------DISAGREEMENT----------\n'),
             write(Out, 'Covid Advice System: I have found the disagreement. User believes '),print_fact(F),
             write(Out, ' is an initial fact,but the computer neither believes nor infers it.'), write(' is an initial fact,but the computer neither believes nor infers it.'),
-            assert(different(F)),!
+            assert(different(F)),!, conversations
         ; 
             \+user_fact(_,F,initial_fact,_),!,
-            write('Covid Advice System: It is not an initial user fact,try again...\n'),
+            write('Covid Advice System: It is not an initial user fact,please select another reason.\n'),whynot(F),
             write(Out,'Covid Advice System: It is not a user initial fact, please select another reason.\n'),nl,fail
         )
     ;    Number =:= 2
-    ->  write(Out, 'Because it is a new fact deduced by a rule'), reason_rule, nl, !
+    ->  write(Out, 'Because it is a new fact deduced by a rule'), reason_rule(F), nl, !
     ;    Number =:= 3
     -> write(Out,'Covid Advice System:Bye\n'),write('Covid Advice System: Bye\n')->halt
     ;
@@ -44,7 +49,7 @@ whynot(F):-
 %% IN this case the computer
 
 
-reason_rule:-
+reason_rule(F):-
     repeat,
     nb_getval(fileOutput,Out),
     write('User: Please select a rule number: '),nl,
@@ -54,15 +59,14 @@ reason_rule:-
     prompt(_, ''),
     read(N),nl,     
     (  
-        user_rule(N, A, F)
+        (   user_rule(N, A, F)
         ->
-        (
+            (
             rule(_,A,F)
         ->
             write(Out,'User:'),
             assert(yr_computer_user(N,A,F)),!, 
             check(A, NL),
-            write(NL),
             pretty_list(NL,_Pretty),
             option_whynot
          ;   
@@ -71,6 +75,11 @@ reason_rule:-
             write(Out, '\n----------DISAGREEMENT----------\n'),
             write(Out, 'Covid Advice System: I found the disagreement! I do not have this rule '),print_rule(N),write(Out, ', but the user has it.'),write(', but the user has it.'), nl,
             assert(different(user_rule(N,_,_))),!
+            )
+        ;   write('Covid Advice System: This rule are not used in deduction, please choose another rule.'),
+            write(Out,'Covid Advice System: This rule are not used in deduction, please choose another rule.'),
+            reason_rule(F)
+
         )
     ;   
         write('Not a valid choice, try again...'), nl, fail).
@@ -85,15 +94,14 @@ option_whynot :-
     write('User:'),
     read(N),
     (   y_computer_user(N, Fact)
-    ->  whynot(Fact)
+    ->  assert(asked_question(Fact)), whynot(Fact)
     ;  n_computer_user(N, Fact)
-    ->  whynot(Fact)
+    ->  assert(asked_question(Fact)),whynot(Fact)
     ).
 
 
 check([],[]).
 check([not(H)|T], N):-
-    write(H),
     \+ deduce_backwards(H, _DAG),!, 
     check(T, N).
 check([H|T], [H|N]):-
